95577c0490e9c78edb23fff8a2e8f22bfb2d3855
KeyForce <july.master@outlook.com>
2020-01-08 19:44:53 +0800

    New Code

--- Domain-Adaptation/PCA.py
+++ Domain-Adaptation/PCA.py
@@ -35,7 +35,7 @@ for i in range(10):
         print(x,y,z)
         c = cm.rainbow(int(255 * i / 9))  # 上色
         ax.text(x, y, z, str(i), backgroundcolor=c)  # 标位子
-    # ax.scatter(number[:, 0], number[:, 1], number[:, 2], label='Number-' + str(i))
+    # ax.scatter(number[:, 0], number[:, 1], number[:, 2], label='Num-' + str(i))
 
 # ax.scatter(num0[:, 0], num0[:, 1], num0[:, 2], c=0,label='Number 0')
 # ax.scatter(new_X[:, 0], new_X[:, 1], new_X[:, 2], c=Y, label='Number')
@@ -47,5 +47,25 @@ ax.set_xlabel('X')
 ax.set_xlim(-30, 30)
 ax.set_ylim(-30, 30)
 ax.set_zlim(-30, 30)
+ax.set_title('MNIST PCA 3D')
 # plt.legend(loc=2)
+
+
+# 二维
+pca = decomposition.PCA(n_components=3)
+Data = pca.fit_transform(X)
+
+fig = plt.figure(2)
+for i in range(10):
+    number = np.array([Data[index] for index, v in enumerate(Y) if v == i])
+    for x, y in zip(number[:, 0], number[:, 1]):
+        c = cm.rainbow(int(255 * i / 9))  # 上色
+        plt.text(x, y, str(i), backgroundcolor=c)  # 标位子
+    # plt.scatter(number[:, 0], number[:, 1],label='Num-' + str(i))
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.title('MNIST PCA 2D')
+plt.xlim(-40, 40)
+plt.ylim(-40, 40)
+# plt.legend(loc=1)
 plt.show()
--- Domain-Adaptation/T-SNE.py
+++ Domain-Adaptation/T-SNE.py
@@ -15,6 +15,8 @@ from sklearn import manifold
 mnist = datasets.load_digits()
 X = mnist.data
 Y = mnist.target
+
+# 三维
 tsne = manifold.TSNE(n_components=3, init='pca', random_state=0)
 Data = tsne.fit_transform(X)
 
@@ -28,10 +30,9 @@ for i in range(10):
     number = np.array([Data[index] for index, v in enumerate(Y) if v == i])
 
     for x, y, z in zip(number[:, 0], number[:, 1], number[:, 2]):
-        print(x, y, z)
         c = cm.rainbow(int(255 * i / 9))  # 上色
         ax.text(x, y, z, str(i), backgroundcolor=c)  # 标位子
-    # ax.scatter(number[:, 0], number[:, 1], number[:, 2], label='Number-' + str(i))
+    # ax.scatter(number[:, 0], number[:, 1], number[:, 2], label='Num-' + str(i))
 
 # 坐标轴
 ax.set_zlabel('Z')
@@ -41,5 +42,24 @@ ax.set_xlabel('X')
 ax.set_xlim(-30, 30)
 ax.set_ylim(-30, 30)
 ax.set_zlim(-30, 30)
-plt.legend(loc=2)
+ax.set_title('MNIST T-SNE 3D')
+# plt.legend(loc=2)
+
+# 二维
+tsne = manifold.TSNE(n_components=2, init='pca', random_state=0)
+Data = tsne.fit_transform(X)
+
+fig = plt.figure(2)
+for i in range(10):
+    number = np.array([Data[index] for index, v in enumerate(Y) if v == i])
+    for x, y in zip(number[:, 0], number[:, 1]):
+        c = cm.rainbow(int(255 * i / 9))  # 上色
+        plt.text(x, y, str(i), backgroundcolor=c)  # 标位子
+    # plt.scatter(number[:, 0], number[:, 1],label='Num-' + str(i))
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.title('MNIST T-SNE 2D')
+plt.xlim(-60, 80)
+plt.ylim(-80, 80)
+# plt.legend()
 plt.show()
